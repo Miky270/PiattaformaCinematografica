674fbf3840fffc3eb8d35db0daca740ae9c4c012
Simodisto <simonesgr2006@gmail.com>
2024-12-15 16:48:11 +0100

    risolto errore legato alla stampa NON ordinata delle raccomandazioni film

--- raccomandazioni.py
+++ raccomandazioni.py
@@ -13,14 +13,17 @@ def analizza_generi_preferiti(catalogo):
     return generi_ordinati
 
 def genera_raccomandazioni(catalogo):
-    generi_preferiti=analizza_generi_preferiti(catalogo)
-    raccomandazioni=[]
-    titoli_aggiunti=set()
+    generi_preferiti = analizza_generi_preferiti(catalogo)
+    raccomandazioni = []
+    titoli_aggiunti = set()
 
     for genere in generi_preferiti:
         for elemento in catalogo:
             if genere in elemento["genere"] and elemento["titolo"] not in titoli_aggiunti:
                 raccomandazioni.append(elemento["titolo"])
                 titoli_aggiunti.add(elemento["titolo"])
-                
+
+    raccomandazioni.sort(key=lambda titolo: next(elemento["visualizzazioni"] for elemento in catalogo if elemento["titolo"] == titolo), reverse=True)
+
     return raccomandazioni
+
